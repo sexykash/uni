880d72bb994ee1447ef181f245ea672009c10780
Sexykash <151502901+sexykash@users.noreply.github.com>
2024-01-28 10:29:04 +0530

    Update play.py

--- SoloCloud/utils/inline/play.py
+++ SoloCloud/utils/inline/play.py
@@ -54,6 +54,12 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "—————————◉"
     buttons = [
         [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
+        [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
